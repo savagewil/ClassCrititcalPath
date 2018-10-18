8b280eb45d24465ba0c9f748856691a9b2677419
Alexander Kneipp <alexander.h.kneipp@gmail.com>
2018-10-18 15:21:23 -0400

    Starts shell code

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from graph import graph
 
+def shell():
+    while command.to_lower() != "q":
+        command = input("> ")
+        if command == "critical path":
+
+        print(command)
+
 def main():
     g = graph()
-    g.readGraph(test.txt)
-    g.add()
+    g.readGraph("test.txt")
+    shell()
 
 if __name__ == "__main__":
     main()
