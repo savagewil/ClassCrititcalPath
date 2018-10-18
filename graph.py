9255737f4869197621c39605938bcdc873dced12
Alexander Kneipp <alexander.h.kneipp@gmail.com>
2018-10-18 10:56:48 -0400

    Adds writing graph in memory to file.

--- graph.py
+++ graph.py
@@ -5,13 +5,18 @@ class graph:
 
     def readGraph(self, fileName):
         for line in open(fileName):
-            print(line)
             nodes = line.split()
             if nodes[0] in self.adjList:
                 if nodes[1] not in self.adjList[nodes[0]]:
                     self.adjList[nodes[0]].append(nodes[1])
             else:
                 self.adjList[nodes[0]] = [nodes[1]]
+
+    def writeToFile(self, fileName):
+        f = open(fileName, "a")
+        for key in self.adjList:
+            for val in self.adjList[key]:
+                f.write(key + " " + val + "\n")
 
     def printAdjList(self):
         for key in self.adjList:
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ def main():
     g = graph()
     g.readGraph("test.txt")
     g.printAdjList()
+    g.writeToFile("test1.txt")
 
 if __name__ == "__main__":
     main()
